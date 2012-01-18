aa582af48e493b7586bb017590d2ec6a0b75e321
Mykal Cuin <mcuin@mines.edu>
2012-01-17 18:45:55 -0700

    Cpmpleted Level 3

--- project02/rubywarrior/mykal-beginner/player.rb
+++ project02/rubywarrior/mykal-beginner/player.rb
@@ -1,8 +1,11 @@
 class Player
   def play_turn(warrior)
     warrior.feel.empty?
+	warrior.health
 	if !warrior.feel.empty?
 		warrior.attack!
+	elsif warrior.health < 20
+		warrior.rest!
 	else
 		warrior.walk!
 	end
