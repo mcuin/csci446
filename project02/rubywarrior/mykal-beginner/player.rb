1f30ff43f78793afc26bd7bb0636bd36b76dcaf1
Mykal Cuin <mcuin@mines.edu>
2012-01-23 13:34:03 -0700

    Level 4 complete

--- project02/rubywarrior/mykal-beginner/player.rb
+++ project02/rubywarrior/mykal-beginner/player.rb
@@ -1,13 +1,22 @@
 class Player
   def play_turn(warrior)
-    warrior.feel.empty?
-	warrior.health
-	if !warrior.feel.empty?
-		warrior.attack!
-	elsif warrior.health < 20
-		warrior.rest!
+	if warrior.health < 20
+		if warrior.health == @health && warrior.health < 20
+			warrior.rest!
+		elsif !warrior.feel.empty?
+			warrior.attack!
+		elsif warrior.health < @health
+			warrior.walk!
+		else 
+			warrior.rest!
+		end
 	else
-		warrior.walk!
+		if !warrior.feel.empty?
+			warrior.attack!
+		else 
+			warrior.walk!
+		end
 	end
+	@health = warrior.health
   end
 end
