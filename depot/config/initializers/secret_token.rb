2cb14ff9611d743b22f0d89e078dff895270a176
Mykal Cuin <mcuin@mines.edu>
2012-01-30 13:02:57 -0700

    ALU passing

--- depot/config/initializers/secret_token.rb
+++ depot/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '78c1698d1d64a1af384abbe4d51744065d07ba0ad3c65fa0a2a0b7a3f358251f3eec4c7cc275d8ff9454a9e782507bc6c736614d3cc259d9560123d326e8409b'
+Depot::Application.config.secret_token = '4256ec6c5a6443aeb7a490084a117d715110f77c548b4f94b1cdbc9ff99c4fe3a718e13cbff36ad6bea7195c014a691ad50971c8e1923bfac25bd6563f5e229c'
