ed2cfd2b0a7c4c418f60b6d88356dbf14deb82bf
Mykal Cuin <mcuin@mines.edu>
2012-03-12 16:48:45 -0600

    Photo database added and raked.

--- project05/project04/db/migrate/20120311182926_create_authors.rb
+++ project05/project04/db/migrate/20120311182926_create_authors.rb
@@ -2,7 +2,6 @@ class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
       t.string :name
-	  t.string :img_url
       t.timestamps
     end
   end
--- project05/project04/db/schema.rb
+++ project05/project04/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120311182926) do
+ActiveRecord::Schema.define(:version => 20120312224613) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(:version => 20120311182926) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
