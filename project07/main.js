e627442b12457ac7fe4908abf15b4634c0dfbfbd
unknown <Jackie@Jackie-msi.(none)>
2012-04-23 16:48:06 -0600

    Random number math has been added. Added function for playback.

--- project07/main.js
+++ project07/main.js
@@ -1,5 +1,5 @@
 var guessesLeft = 10;
-var number = 43;
+var number = Math.round(Math.random()*(99)+1);
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
 
 function guessCall() {
@@ -7,6 +7,12 @@ function guessCall() {
   guessesLeft--;
   if (guess == number) {
     alert("You have guessed the correct answer!");
+	var name = prompt("Please enter your name or nickname for the high scores.");
+	$('div#highScores').empty();
+	highScores.push([guessesLeft, name]);
+	populateHighScores(highScores);
+	scores.sort();
+	sort.reverse();
   }
   if (guess > number) {
     alert("Your guess is too high!");
@@ -19,6 +25,12 @@ function guessCall() {
   }	
   updateScore(guessesLeft);
 }
+
+function playAgain() {
+  guessesLeft = 10;
+  number = Math.round(Math.random()*(99)+1);
+}
+
 $(function() {
   updateScore(guessesLeft);
   populateHighScores(highScores);
