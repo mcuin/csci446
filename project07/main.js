83ffa2aaaa8c989b3647df5ffa69ba4a5576781e
unknown <Jackie@Jackie-msi.(none)>
2012-04-23 12:44:32 -0600

    Guess function call working and giving basic popups for high low and correct.

--- project07/main.js
+++ project07/main.js
@@ -1,5 +1,23 @@
 var guessesLeft = 10;
+var number = 43;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+
+function guessCall() {
+  var guess = document.forms.guessTheNumber.guess.value;
+  if (guess == number) {
+    alert("You have guessed the corret answer!");
+  }
+  if (guess > number) {
+    alert("Your guess is too high!");
+  }
+  if (guess < number) {
+    alert("Your guess is too low!");
+  }
+  guessesLeft--;
+  if (guessesLeft == 0) {
+    $("You have no more guesses left.")
+  }	
+}
 
 $(function() {
   updateScore(guessesLeft);
